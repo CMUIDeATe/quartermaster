2023c0aed4829e752b5f2bbf35d5ae86c893a517
Tim Parenti <tparenti@andrew.cmu.edu>
2016-08-02 22:42:56 -0400

    Adds Item seeds for basic preliminary inventory.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Initial user with temporary throwaway password
+# Clear data
 User.delete_all()
+Tag.delete_all()
+Type.delete_all()
+Item.delete_all()
+
+# Initial user with temporary throwaway password
 User.create(email: 'tparenti@andrew.cmu.edu', password: 'nachos', password_confirmation: 'nachos')
+
+# Basic preliminary inventory
+# From lines 4-7 of Physical Computing inventory as of 2016-08-02
+types = Type.create([ { name: 'Consumable' }, { name: 'Local' } ])
+
+tags1 = Tag.create([ { name: 'Audio' }, { name: 'Recording' } ])
+item1 = Item.create( name: 'Audio Mixer', description: '16-channel, 4-bus Mixer with Built-in Effects and USB', minimum_quantity: 0, current_quantity: 1, tags: tags1, types: [types[1]] )
+
+tags2 = Tag.create([ { name: 'Adapter' }, { name: 'Cables' } ])
+tags2 << tags1[0]
+item2 = Item.create( name: 'XLR Cable', description: 'XLR cable to 1/4in TRS, 5 foot', minimum_quantity: 0, current_quantity: 2, tags: tags2 )
+
+tags3 = Tag.create([ { name: 'Actuators' }, { name: 'Drivers' }, { name: 'Sensors' } ])
+item3 = Item.create( name: 'Arduino CNC Shield (GRBL Compatible)', current_quantity: 0, tags: tags3, types: types )
+
+tags4 = Tag.create([ { name: 'Electronics' }, { name: 'LED' }, { name: 'Lighting' } ])
+item4 = Item.create( name: 'LED, 5mm Infrared 850nm', current_quantity: 0, sale_price: 0.36, tags: tags4, types: [types[1]] )
