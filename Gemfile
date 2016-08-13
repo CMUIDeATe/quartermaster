9119f33ae6260a56b8f019bed852aec108d38e97
Tim Parenti <tparenti@andrew.cmu.edu>
2016-08-13 18:27:44 -0400

    Adds Gravatars in nav with gravtastic.

--- Gemfile
+++ Gemfile
@@ -56,4 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'devise', '~> 4.2.0'
+gem 'gravtastic', '~> 3.2.6'
 gem 'paperclip', '~> 5.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  include Gravtastic
+  gravtastic secure: true, size: 20, rating: 'G', default: 'identicon'
+
   def andrewid
     if self.email.ends_with? "@andrew.cmu.edu"
       "#{self.email.gsub("@andrew.cmu.edu", "")}"
