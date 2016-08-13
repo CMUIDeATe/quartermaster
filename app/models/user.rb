eba60f62876e884fdbf1487e9396c56d9353682a
Tim Parenti <tparenti@andrew.cmu.edu>
2016-08-13 18:09:44 -0400

    Implements an andrewid method for users.
    Truncates "@andrew.cmu.edu" where possible; otherwise, displays full email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def andrewid
+    if self.email.ends_with? "@andrew.cmu.edu"
+      "#{self.email.gsub("@andrew.cmu.edu", "")}"
+    else
+      "#{self.email}"
+    end
+  end
+
 end
