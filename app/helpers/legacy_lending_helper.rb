eaf713885aa4d041d74de4de30cd52f8ac111bd1
Tim Parenti <tparenti@andrew.cmu.edu>
2016-08-24 15:58:29 -0400

    Adds pill-based navigation to legacy lending pages.

--- app/helpers/legacy_lending_helper.rb
+++ app/helpers/legacy_lending_helper.rb
@@ -1,2 +1,10 @@
 module LegacyLendingHelper
+
+  def nav_link(text, path)
+    options = current_page?(path) ? { class: "active" } : {}
+    content_tag(:li, options) do
+      link_to text, path
+    end
+  end
+
 end
