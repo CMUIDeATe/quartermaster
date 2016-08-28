a61b911db7154ba38a0d6727c20f26493ea38882
Tim Parenti <tparenti@andrew.cmu.edu>
2016-08-28 17:57:10 -0400

    Fixes application controller filter for authentication.
    Per devise instructions for Rails 5.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, alert: exception.message
