1ffcacd83d48eb1d63b4204a5299b0ba1afb2970
Tim Parenti <tparenti@andrew.cmu.edu>
2017-06-30 16:34:22 -0400

    Updates university holidays for FY2019.
    Adds:
    * FY2019 university holidays (July 2018 - June 2019)
    * The extra winter holidays from FY2017 (December 2016)

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -7,19 +7,22 @@ BusinessTime::Config.work_week = [:mon, :tue, :wed, :thu, :fri]
 # Official Carnegie Mellon University holidays
 # https://www.cmu.edu/hr/benefits/benefit_programs/time_off/holidays.html
 
-# FY 2016-2017
+# FY2017
 BusinessTime::Config.holidays << Date.parse("04 Jul 2016")
 BusinessTime::Config.holidays << Date.parse("05 Sep 2016")
 BusinessTime::Config.holidays << Date.parse("24 Nov 2016")
 BusinessTime::Config.holidays << Date.parse("25 Nov 2016")
 BusinessTime::Config.holidays << Date.parse("23 Dec 2016")
 BusinessTime::Config.holidays << Date.parse("26 Dec 2016")
+BusinessTime::Config.holidays << Date.parse("27 Dec 2016") # additional winter holiday
+BusinessTime::Config.holidays << Date.parse("28 Dec 2016") # additional winter holiday
+BusinessTime::Config.holidays << Date.parse("29 Dec 2016") # additional winter holiday
 BusinessTime::Config.holidays << Date.parse("30 Dec 2016")
 BusinessTime::Config.holidays << Date.parse("02 Jan 2017")
 BusinessTime::Config.holidays << Date.parse("16 Jan 2017")
 BusinessTime::Config.holidays << Date.parse("29 May 2017")
 
-# FY 2017-2018
+# FY2018
 BusinessTime::Config.holidays << Date.parse("04 Jul 2017")
 BusinessTime::Config.holidays << Date.parse("04 Sep 2017")
 BusinessTime::Config.holidays << Date.parse("23 Nov 2017")
@@ -30,4 +33,16 @@ BusinessTime::Config.holidays << Date.parse("01 Jan 2018")
 BusinessTime::Config.holidays << Date.parse("02 Jan 2018")
 BusinessTime::Config.holidays << Date.parse("15 Jan 2018")
 BusinessTime::Config.holidays << Date.parse("28 May 2018")
+
+# FY2019
+BusinessTime::Config.holidays << Date.parse("04 Jul 2018")
+BusinessTime::Config.holidays << Date.parse("03 Sep 2018")
+BusinessTime::Config.holidays << Date.parse("22 Nov 2018")
+BusinessTime::Config.holidays << Date.parse("23 Nov 2018")
+BusinessTime::Config.holidays << Date.parse("24 Dec 2018")
+BusinessTime::Config.holidays << Date.parse("25 Dec 2018")
+BusinessTime::Config.holidays << Date.parse("31 Dec 2018")
+BusinessTime::Config.holidays << Date.parse("01 Jan 2019")
+BusinessTime::Config.holidays << Date.parse("21 Jan 2019")
+BusinessTime::Config.holidays << Date.parse("27 May 2019")
 
