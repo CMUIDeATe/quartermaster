021f717badaacb2441b8f4826a89ded0390d91e5
Tim Parenti <tparenti@andrew.cmu.edu>
2017-07-19 22:09:03 -0400

    Adds application version (commit hash) to nav for logged-in users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def app_version
+    if Rails.env.development?
+      begin
+        %x{git log --pretty=format:"%h" -n1}
+      rescue
+        "?"
+      end
+    else
+      File.read(Rails.root.join("REVISION"))[0..6]
+    end
+  end
+
 end
