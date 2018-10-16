cd40cd95acadb055e58b4464b095511d94d12431
Tim Parenti <tparenti@andrew.cmu.edu>
2018-10-16 10:58:38 -0400

    Add available software link to navigation.

--- app/helpers/legacy_lending_helper.rb
+++ app/helpers/legacy_lending_helper.rb
@@ -1,9 +1,9 @@
 module LegacyLendingHelper
 
-  def nav_link(text, path)
+  def nav_link(text, path, link_options = {})
     options = current_page?(path) ? { class: "active" } : {}
     content_tag(:li, options) do
-      link_to text, path
+      link_to text, path, link_options
     end
   end
 
