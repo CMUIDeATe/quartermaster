15c0f3f8170f58600bbe912a80ca200f7267b87d
Tim Parenti <tparenti@andrew.cmu.edu>
2019-11-12 15:26:09 -0500

    Add reservations info, where applicable, to public room schedules.
    In particular, A10A can be reserved directly by authorized users.  For other
    classrooms (A5, A10, 106B, 106C), non-administrative users should generally
    send email.  Don't show any such info for rooms that aren't nominally
    reservable.

--- app/controllers/legacy_lending_controller.rb
+++ app/controllers/legacy_lending_controller.rb
@@ -102,42 +102,52 @@ class LegacyLendingController < ApplicationController
   end
 
   def schedules
-    room = params[:room]
-    room ||= ''
+    @room = params[:room]
+    @room ||= ''
 
     @mrbs_room = nil
-    case room.upcase()
+    case @room.upcase()
       when "A5"
         @mrbs_area = 4
         @room_name = "Experimental Fabrication (A5)"
+        @room_full_name = "the Experimental Fabrication Lab"
       when "A5B"
         @mrbs_area = 12
         @room_name = "Laser Cutters (A5B)"
+        @room_full_name = "the IDeATe laser cutters"
       when "A10"
         @mrbs_area = 6
         @room_name = "Physical Computing (A10)"
+        @room_full_name = "the Physical Computing Lab"
       when "A10A"
         @mrbs_area = 5
         @room_name = "Media Lab (A10A)"
+        @room_full_name = "the Media Lab"
       when "A29"
         @mrbs_area = 8
         @room_name = "Lending Desk (A29)"
+        @room_full_name = "the IDeATe Lending Desk"
       when "A30"
         @mrbs_area = 10
         @room_name = "Wood Shop (A30)"
+        @room_full_name = "the IDeATe Wood Shop"
       when "A31"
         @mrbs_area = 13
         @room_name = "Large Project Build Space (A31)"
+        @room_full_name = "the Large Project Build Space"
       when "106B"
         @mrbs_area = 7
         @mrbs_room = 58
         @room_name = "IDeATe Studio A (106B)"
+        @room_full_name = "IDeATe Studio A"
       when "106C"
         @mrbs_area = 7
         @mrbs_room = 59
         @room_name = "IDeATe Studio B (106C)"
+        @room_full_name = "IDeATe Studio B"
+      when "106C"
       else
-        flash[:alert] = "Room #{room} does not exist."
+        flash[:alert] = "Room #{@room} does not exist."
         redirect_to root_path
     end
 
